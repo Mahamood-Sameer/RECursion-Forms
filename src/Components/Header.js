49b6f936ea61b8e6a2707715e129ba9939b45908
Mahamood-Sameer <d.mahamoodsameer786@gmail.com>
2021-12-07 17:41:57 +0530

    Added a menu to profile

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -8,14 +8,22 @@ import AppsIcon from "@mui/icons-material/Apps";
 import { Link } from "react-router-dom";
 import { auth, provider } from "../Firebase/firebase";
 
+// Profile options
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import Divider from "@mui/material/Divider";
+import Tooltip from "@mui/material/Tooltip";
+import Settings from "@mui/icons-material/Settings";
+import Logout from "@mui/icons-material/Logout";
+
 function Header() {
   const [user, setUser] = useState(null);
   const LogIn = () => {
     auth
       .signInWithPopup(provider)
       .then((result) => {
-        setUser(result);
-        console.log(result);
+        setUser(result.user);
       })
       .catch((err) => {
         alert(err.message);
@@ -35,60 +43,139 @@ function Header() {
     }
   }, [user]);
 
-  return (
-    <div className="header">
-      <div className="header__left">
-        <IconButton>
-          <MenuIcon />
-        </IconButton>
-        <Link to="/">
-          <DescriptionIcon className="Document-image" />
-        </Link>
-        <Link to="/" className="homeLink">
-          RECursion Forms
-        </Link>
-      </div>
-      <div className="header__middle">
-        <IconButton>
-          <SearchIcon />
-        </IconButton>
-        <input type="text" placeholder="Search" />
-      </div>
-      <div className="header__right">
-        <IconButton>
-          <AppsIcon />
-        </IconButton>
-        {user ? (
-          <>
-            <Avatar className="profile_pic" src={user?.user?.photoURL} />
-            <Link
-              to={`/${user?.uid}/myforms`}
-              style={{ textDecoration: "none" }}
-            >
-              <Button className="buttons">My forms</Button>
-            </Link>
+  // Profile popup
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
+  return (
+    <>
+      <div className="header">
+        <div className="header__left">
+          <IconButton>
+            <MenuIcon />
+          </IconButton>
+          <Link to="/">
+            <DescriptionIcon className="Document-image" />
+          </Link>
+          <Link to="/" className="homeLink">
+            RECursion Forms
+          </Link>
+        </div>
+        <div className="header__middle">
+          <IconButton>
+            <SearchIcon />
+          </IconButton>
+          <input type="text" placeholder="Search" />
+        </div>
+        <div className="header__right">
+          <IconButton>
+            <AppsIcon />
+          </IconButton>
+          {user ? (
+            <>
+              <Tooltip title="Account">
+                <Avatar
+                  onClick={handleClick}
+                  className="profile_pic"
+                  src={user?.photoURL}
+                />
+              </Tooltip>
+              
+                <Settings style={{marginLeft:"20px"}} />
+              
+            </>
+          ) : (
             <Button
               className="buttons"
               onClick={() => {
-                LogOut();
+                LogIn();
               }}
             >
-              Logout
+              Login
             </Button>
-          </>
-        ) : (
-          <Button
-            className="buttons"
-            onClick={() => {
-              LogIn();
-            }}
-          >
-            Login
-          </Button>
-        )}
+          )}
+        </div>
       </div>
-    </div>
+      {/* Menu card on clicking the profile */}
+      <Menu
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        onClick={handleClose}
+        PaperProps={{
+          elevation: 0,
+          sx: {
+            overflow: "visible",
+            filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
+            mt: 1.5,
+            "& .MuiAvatar-root": {
+              width: 32,
+              height: 32,
+              ml: -0.5,
+              mr: 1,
+            },
+            "&:before": {
+              content: '""',
+              display: "block",
+              position: "absolute",
+              top: 0,
+              right: 14,
+              width: 10,
+              height: 10,
+              bgcolor: "background.paper",
+              transform: "translateY(-50%) rotate(45deg)",
+              zIndex: 0,
+            },
+          },
+        }}
+        transformOrigin={{ horizontal: "right", vertical: "top" }}
+        anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
+      >
+        <MenuItem>
+          Hii ,{" "}
+          <i style={{ fontSize: "12px", fontWeight: "600", marginLeft: "5px" }}>
+            {" "}
+            {user?.displayName}
+          </i>
+        </MenuItem>
+        <MenuItem>
+          <span
+            style={{ fontSize: "12px", fontWeight: "600", marginLeft: "5px" }}
+          >
+            {" "}
+            {user?.email}
+          </span>
+        </MenuItem>
+        <Divider />
+        <Link
+          to={`/${user?.uid}/myforms`}
+          style={{ textDecoration: "none", color: "black" }}
+        >
+          <MenuItem>
+            <ListItemIcon>
+              <DescriptionIcon />
+            </ListItemIcon>
+            My Forms
+          </MenuItem>
+        </Link>
+        <MenuItem
+          onClick={() => {
+            LogOut();
+          }}
+        >
+          <ListItemIcon>
+            <Logout fontSize="small" />
+          </ListItemIcon>
+          Logout
+        </MenuItem>
+      </Menu>
+    </>
   );
 }
 
